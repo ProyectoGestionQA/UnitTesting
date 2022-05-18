c3080b24c03ae535bafa29c148dab7beb17e1610
PabloCanelas <54004206+PabloCanelas@users.noreply.github.com>
2022-05-18 18:42:09 -0400

    Initial commit

---
+++ src/js/class/Counter/Counter.js
@@ -0,0 +1,28 @@
+class Counter {
+    constructor(id) {
+        //this.text = document.getElementById(id);
+        this.counter = 0;
+    }
+
+    init(counter = 0) {
+        this.counter = counter;
+        //this.updateText();
+    }
+
+    increaseCounter() {
+        this.counter += 1;
+        //this.updateText();
+        return this.counter
+    }
+
+    decreaseCounter() {
+        this.counter -= 1;
+        this.updateText();
+    }
+
+    updateText() {
+        this.text.innerHTML = `${this.counter}`;
+    }
+}
+
+export default Counter;
